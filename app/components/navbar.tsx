32b4203cb66a6bbb4895d472280c5c3e7b254f75
kiran <kiranmavi5502@gmail.com>
2023-10-09 12:47:58 +0530

    setup and navbar initiate

---
+++ app/components/navbar.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import '../styles/styles.css';
+
+export default function Navbar(){
+    return (
+       <nav className='navbar'>
+        <div><img src="logo.png"/></div>
+       </nav>
+    )
+}
