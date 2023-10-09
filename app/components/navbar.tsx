b1fe56464a61eb7e5e4dbc08402b8b169931aad4
kiran <kiranmavi5502@gmail.com>
2023-10-09 14:22:06 +0530

    navbar changes and banner ekesh

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,6 +6,19 @@ export default function Navbar(){
     return (
        <nav className='navbar'>
         <div><img src="logo.png"/></div>
+        <div className='navbarItemsBox'>
+            <div>
+            <ul className="flex flex-row md:gap-8">
+                <li className="navbarItems">Home</li>
+                <li className="navbarItems">About Us</li>
+                <li className="navbarItems">Services</li>
+                <li className="navbarItems">Case Studies</li>
+                <li className="navbarItems">Career</li>
+                <li className="navbarItems">Blog</li>
+            </ul>
+            </div>
+            <div className='contactButton'>Contact Us</div>
+        </div>
        </nav>
     )
 }
