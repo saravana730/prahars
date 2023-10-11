7f922c5ed01b63ec5c1227f3ea4f37e8204f8c58
Dev Veract <dev@veractconsultancy.in>
2023-10-11 17:28:22 +0530

    width issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='LandingPage flex'>
+        <div className='LandingPage'>
         <Navbar/>
         <Banner />
         </div>
