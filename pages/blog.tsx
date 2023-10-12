44eae0762ebef745fa5ffff36ea406d7b38bfa6c
Ekesh <ekeshr2@gmail.com>
2023-10-12 09:04:11 +0530

    services

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -14,43 +14,44 @@ export default function Blogs() {
             <div className="blog_content">
                 Know why at <br />
                 <span style={{color:"#df0070"}}>our blogs</span>
-                <div className="row">
-                    <div className="col">
-                        <div className="row">
-                            <div className="col">
-                                <div className="bg_image_1"></div>
-                                    <div className="blog_title">Paid Marketing Vs Affiliate Marketing</div>
-                                        <div className="blog_desc">Paid marketing and affiliate marketing are two of the<br/> most popular ways to promote products and services<br/> online.</div>
-                                            <div className="read_more_justify">
-                                                <div className="read_more_row">Read More</div>
-                                                <div className="read_more_arrow"><Image src={forward_arrow} alt='>' width={20} height={20} /></div>
-                                            </div>
-                            </div>
-                            <div className="right_side_blog">
+                <div className="blog_padding_page">
+                    <div className="row">
+                        <div className="col">
+                            <div className="row">
                                 <div className="col">
-                                    <div className="bg_image_2"></div>
-                                        <div className="blog_title">Social Media Marketing - A Guide</div>
-                                            <div className="blog_desc">Social media marketing is one of the most efficient and<br/>
-                                             effective ways to connect with customers and promote<br/> 
-                                             your brand.
-                                            </div>
-                                            <button>
+                                    <div className="bg_image_1"></div>
+                                        <div className="blog_title">Paid Marketing Vs Affiliate Marketing</div>
+                                            <div className="blog_desc">Paid marketing and affiliate marketing are two of the<br/> most popular ways to promote products and services<br/> online.</div>
+                                                {/* <button className="outlined-button_blog"> */}
                                                     <div className="read_more_justify">
-                                                        <div className="blog_outlined_button">Read More</div>
-                                                        {/* <div className="blog_outlined_button">for more details...</div> */}
-                                                        <div className="blog_img_padding"><Image src={forward_arrow} alt='>' width={20} height={20} /></div>
+                                                        <div className="read_more_row">Read More</div>
+                                                        <div className="read_more_arrow"><Image src={forward_arrow} alt='>' width={20} height={20} /></div>
                                                     </div>
-                                            </button>
+                                                {/* </button> */}
+                                </div>
+                                <div className="right_side_blog">
+                                    <div className="col">
+                                        <div className="bg_image_2"></div>
+                                            <div className="blog_title">Social Media Marketing - A Guide</div>
+                                                <div className="blog_desc">Social media marketing is one of the most efficient and<br/>
+                                                effective ways to connect with customers and promote<br/> 
+                                                your brand.
+                                                </div>
+                                                <div className="read_more_justify">
+                                                    <div className="read_more_row">Read More</div>
+                                                    <div className="read_more_arrow"><Image src={forward_arrow} alt='>' width={20} height={20} /></div>
+                                                </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        <div className="row">
-                            <div className="bg_image_"></div>
+                        <div className="col">
+                            <div className="row">
+                                <div className="bg_image_"></div>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </div>
