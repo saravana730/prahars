8521555586a2287eabad70a63b23ddc44cd38d68
kiran <kiranmavi5502@gmail.com>
2023-10-20 13:20:44 +0530

    email and password validation ion login page

--- app/admin/login/login.tsx
+++ app/admin/login/login.tsx
@@ -1,25 +1,64 @@
 
 "use client";
+import React, { useState } from 'react';
 import { login } from '@/services/authService';
-
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Login = async () => {
+    
+    let email="";
+    let password="";
+
+    const handleEmail=(e: { target: { value: string; }; })=>{
+        email= e.target.value  
+    }
+    const handlePassword=(e:{target :{ value:string}})=>{
+        password=e.target.value;
+    }
+    function emailValidation(emailCheck:string){
+        const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        return emaiPattern.test(emailCheck);
+    }
+    function passwordValidation(passwordCheck:string){
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return passwordPattern.test(passwordCheck);
+    }
     function callLogin() {
-
-        login("saravanadev@gmail.com", "test").then((data) => {
-            console.log(data);
-        }
-
-        ).catch(
-            (e) => {
-                console.log(e);
+        
+        if(email && password.length !==0){
+            const isEmailValid=emailValidation(email);
+            const isPasswordValid=passwordValidation(password);
+            if(isEmailValid === true && isPasswordValid === false){
+                toast.error("Please enter the correct password")
             }
-        )
+            else if(isEmailValid === false && isPasswordValid === true){
+                toast.error("Please enter the correct email address")
+            }   
+            else{
+                console.log(isEmailValid,isPasswordValid);
+                login(email, password).then((data) => {
+                    console.log(data);
+                    toast.success("welcome to prahars");
+                }
 
+                ).catch(
+                    (e) => {
+                        console.log(e);
+                        toast.error("Api error");
+                    }
+                   
+                )
+            }
+        }else{
+            toast.error("Please fill the details")
+        }
     }
 
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+             <ToastContainer />
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img className="mx-auto h-10 w-auto" src="\logo.png" alt="Praghas">
                 </img>
@@ -31,7 +70,7 @@ export const Login = async () => {
                     <div>
                         <label className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                         <div className="mt-2">
-                            <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <input id="email" name="email" type="email" onChange={handleEmail} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </input>
                         </div>
                     </div>
@@ -45,7 +84,7 @@ export const Login = async () => {
                             </div>
                         </div>
                         <div className="mt-2">
-                            <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <input id="password" name="password" type="password" onChange={handlePassword} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </input>
                         </div>
                     </div>
